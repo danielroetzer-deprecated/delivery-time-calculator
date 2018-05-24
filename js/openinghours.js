8f67572668dca5a9f4ede6b256cdb1113cf94e3e
Daniel Work-PC <roetzer.daniel@gmail.com>
2018-05-24 15:46:04 +0200

    Added usabillity to set a new date with input fields

---
+++ js/openinghours.js
@@ -0,0 +1,51 @@
+//defines the opening hours of a restaurant
+//from monday to sunday -> from 1 to 7
+var openinghours = {
+  "1": [
+    {
+      "from": "10:30",
+      "to": "14:00"
+    },
+    {
+      "from": "17:00",
+      "to": "22:00"
+    }
+  ],
+  "2": [
+    {
+      "from": "10:30",
+      "to": "14:00"
+    },
+    {
+      "from": "17:00",
+      "to": "22:00"
+    }
+  ],
+  "4": [
+    {
+      "from": "12:00",
+      "to": "23:30"
+    }
+  ],
+  "5": [
+    {
+      "from": "12:00",
+      "to": "23:30"
+    }
+  ],
+  "6": [
+    {
+      "from": "12:00",
+      "to": "23:30"
+    }
+  ],
+  "7": [
+    {
+      "from": "10:00",
+      "to": "15:00"
+    }
+  ]
+}
+
+//defines the deliverytime
+var deliverytime = "50";
